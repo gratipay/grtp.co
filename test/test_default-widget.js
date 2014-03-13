cd2a16f92cc7ec57834a82b5c7f17c83963e5c74
rummik <r@9k1.us>
2014-03-13 15:13:38 -0400

    Add tests for the default widget

---
+++ test/test_default-widget.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+    'readystatus should be ready': function(test) {
+        test.open('http://localhost:9537/test/default-widget.html')
+            .wait(500)
+            .assert.attr('[data-gittip-username]', 'data-gittip-readystatus').is('ready', 'readystatus should be ready')
+            .done();
+    },
+
+    'it should fill the standard values': function(test) {
+        test.open('http://localhost:9537/test/default-widget.html')
+            .wait(500)
+            .assert.exists('iframe')
+            .toFrame('iframe')
+                .assert.exists('.gittip-identity')
+                .assert.text('.gittip-identity').is.not('', 'identity should not be empty')
+                .assert.exists('.gittip-receiving')
+                .assert.text('.gittip-receiving').is.not('', 'receiving should not be empty')
+            .toParent()
+            .done();
+    },
+
+};
