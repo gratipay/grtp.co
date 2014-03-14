66991e50778669a5b382840e1c1c70dfe4d34e6a
rummik <r@9k1.us>
2014-03-14 10:57:44 -0400

    Speed up tests by waiting for an element instead of a specified period

--- test/test_custom-widget.js
+++ test/test_custom-widget.js
@@ -2,14 +2,18 @@ module.exports = {
 
     'readystatus should be ready': function(test) {
         test.open('http://localhost:9537/test/custom-widget.html')
-            .wait()
+            .waitFor(function() {
+                return !!document.querySelector('[data-gittip-readystatus="ready"]');
+            }, [], 2000)
             .assert.attr('[data-gittip-username]', 'data-gittip-readystatus').is('ready', 'readystatus should be ready')
             .done();
     },
 
     'it should fill the standard values': function(test) {
         test.open('http://localhost:9537/test/custom-widget.html')
-            .wait()
+            .waitFor(function() {
+                return !!document.querySelector('[data-gittip-readystatus="ready"]');
+            }, [], 2000)
             .assert.text('.gittip-username').is.not('', 'username should not be empty')
             .assert.text('.gittip-identity').is.not('', 'identity should not be empty')
             .assert.text('.gittip-giving').is.not('', 'giving should not be empty')
--- test/test_default-widget.js
+++ test/test_default-widget.js
@@ -2,14 +2,18 @@ module.exports = {
 
     'readystatus should be ready': function(test) {
         test.open('http://localhost:9537/test/default-widget.html')
-            .wait(500)
+            .waitFor(function() {
+                return !!document.querySelector('[data-gittip-readystatus="ready"]');
+            }, [], 2000)
             .assert.attr('[data-gittip-username]', 'data-gittip-readystatus').is('ready', 'readystatus should be ready')
             .done();
     },
 
     'it should fill the standard values': function(test) {
         test.open('http://localhost:9537/test/default-widget.html')
-            .wait(500)
+            .waitFor(function() {
+                return !!document.querySelector('[data-gittip-readystatus="ready"]');
+            }, [], 2000)
             .assert.exists('iframe')
             .toFrame('iframe')
                 .assert.exists('.gittip-identity')
