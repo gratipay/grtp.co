9f7fe20ceb863aa8e22a0879e6fa8e4dffe07586
Rohit Paul Kuruvilla <rohitpaul@iitrpr.ac.in>
2014-08-29 21:05:41 +0530

    Widgets now work with legacy gittip-* attributes

---
+++ test/test_default-widget-legacy.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+    'readystatus should be ready': function(test) {
+        test.open('http://localhost:9537/test/default-widget-legacy.html')
+            .waitFor(function() {
+                return !!document.querySelector('[data-gratipay-readystatus="ready"]');
+            }, [], 2000)
+            .assert.attr('[data-gittip-username]', 'data-gratipay-readystatus').is('ready', 'readystatus should be ready')
+            .done();
+    },
+
+    'it should fill the standard values': function(test) {
+        test.open('http://localhost:9537/test/default-widget-legacy.html')
+            .waitFor(function() {
+                return !!document.querySelector('[data-gratipay-readystatus="ready"]');
+            }, [], 2000)
+            .assert.exists('iframe')
+            .toFrame('iframe')
+                .assert.exists('.gratipay-identity')
+                .assert.text('.gratipay-identity').is.not('', 'identity should not be empty')
+                .assert.exists('.gratipay-receiving')
+                .assert.text('.gratipay-receiving').is.not('', 'receiving should not be empty')
+            .toParent()
+            .done();
+    },
+
+};
