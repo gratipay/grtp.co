c2dc6640aeb376a445c3c1b2d91f91830528f87b
rummik <r@9k1.us>
2014-08-31 12:01:58 -0400

    [#53] Add unit tests

---
+++ test/test_expose-api-to-iframe.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    'widget should have Gratipay defined': function(test) {
+        test.open('http://localhost:9537/test/expose-api-to-iframe.html')
+            .waitFor(function() {
+                return !!document.querySelector('[data-gratipay-readystatus="ready"]');
+            }, [], 2000)
+            .toFrame('iframe')
+                .execute(function() {
+                    this.assert.ok(typeof window.Gratipay != 'undefined', 'window.Gratipay should not be undefined');
+                })
+            .done();
+    },
+
+};
