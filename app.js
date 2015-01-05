5194cdd40e4d3f9d3f44b04a02ab47f45bb12105
rummik <r@9k1.us>
2015-01-05 01:46:21 -0500

    Add a chart widget

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var engines = require('consolidate');
 var request = require('request');
+var async = require('async');
 var im = require('imagemagick-stream');
 var app = express();
 
@@ -13,22 +14,60 @@ app.get('/v2/:username/button.svg', function(req, res) {
     res.render('button.svg', req.params);
 });
 
+function get(endpoint) {
+    return function(callback) {
+        request.get(
+            {
+                json: true,
+                url: 'https://gratipay.com/' + endpoint
+            },
+
+            function(error, response, data) {
+                if (error || response.statusCode !== 200) {
+                    callback(error || response.statusCode);
+                } else {
+                    callback(null, data);
+                }
+            }
+        );
+    };
+}
+
 app.get('/v2/:username/:widget.svg', function(req, res) {
-    request.get(
+    async.parallel(
         {
-            json: true,
-            url: 'https://gratipay.com/' + req.params.username + '/public.json'
+            public: get(req.params.username + '/public.json'),
+            charts: get(req.params.username + '/charts.json')
         },
 
-        function(error, response, data) {
-            if (error) {
-                return res.status(500).send();
-            } else if (response.statusCode !== 200) {
-                return res.status(404).send();
+        function(err, results) {
+            if (err) {
+                res.status(typeof err == 'number' ? 404 : 500).send();
+                return;
             }
 
+            var data = results.public;
+            var maxReceived = 0;
+            var maxPatrons = 0;
+
+            data.charts = results.charts.filter(function(chart) {
+                maxReceived = Math.max(chart.receipts, maxReceived);
+                maxPatrons = Math.max(chart.patrons, maxPatrons);
+                return !chart.ts_start; // jshint ignore:line
+            });
+
+            data.max_received = maxReceived; // jshint ignore:line
+            data.max_patrons = maxPatrons; // jshint ignore:line
+
             res.set('Content-Type', 'image/svg+xml');
-            res.render(req.params.type + '.svg', data);
+            res.render(req.params.widget + '.svg', data, function(err, result) {
+                if (!err) {
+                    res.send(result);
+                } else {
+                    res.set('Content-Type', '');
+                    res.status(404).send();
+                }
+            });
         }
     );
 });
