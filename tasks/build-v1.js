eecd1b7a456c46ffd7ff2c866aa6053fa668a2e5
rummik <r@9k1.us>
2015-02-02 04:26:19 -0500

    Move devDependencies into dependencies
    This really isn't optimal, but it should help keep things simple for
    now.

--- tasks/build-v1.js
+++ tasks/build-v1.js
@@ -2,19 +2,10 @@ var gulp = require('gulp');
 var moment = require('moment');
 
 module.exports = function() {
-    var stream = gulp.src('lib/**/*.js');
-
-    if (gulp.plugin.cached) {
-        stream.pipe(gulp.plugin.cached('uglify'));
-    }
-
-    stream
+    return gulp.src('lib/**/*.js')
+        .pipe(gulp.plugin.cached('uglify'))
         .pipe(gulp.plugin.uglify())
-        .pipe(gulp.plugin.header(moment().format('/*! [grtp.co] YYYY-MM-DD */[\n]')));
-
-    if (gulp.plugin.cached) {
-        stream.pipe(gulp.plugin.remember('uglify'));
-    }
-
-    return stream.pipe(gulp.dest('www'));
+        .pipe(gulp.plugin.header(moment().format('/*! [grtp.co] YYYY-MM-DD */[\n]')))
+        .pipe(gulp.plugin.remember('uglify'))
+        .pipe(gulp.dest('www'));
 };
