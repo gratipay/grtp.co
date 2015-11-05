c6bf2f4a4a3f3a545a30f617c294d262dcfbc292
anatoly techtonik <techtonik@gmail.com>
2015-11-05 10:58:58 +0300

    Use 4 spaces indent for gulpfile.js itself

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,30 @@ var gulp   = require('gulp');
 var eslint = require('gulp-eslint');
 
 var paths = {
-  scripts: [
-    'gulpfile.js',
-    'lib/**/*.js',
-    'test/**/*.js'
-  ]
+    scripts: [
+        'gulpfile.js',
+        'lib/**/*.js',
+        'test/**/*.js'
+    ]
 };
 
 function lint() {
-  return gulp.src(paths.scripts)
-     // eslint() attaches the lint output to the eslint property 
-     // of the file object so it can be used by other modules. 
-    .pipe(eslint())
-    // eslint.format() outputs the lint results to the console. 
-    .pipe(eslint.format())
-    // To have the process exit with an error code (1) after
-    // lint errors, return the stream and pipe to failAfterError
-    // last. 
-    //.pipe(eslint.failAfterError())
-    ;
+    return gulp.src(paths.scripts)
+        // eslint() attaches the lint output to the eslint property 
+        // of the file object so it can be used by other modules. 
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console. 
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) after
+        // lint errors, return the stream and pipe to failAfterError
+        // last. 
+        //.pipe(eslint.failAfterError())
+        ;
 }
 
 // Watch files for changes to rerun tasks
 function watch() {
-  gulp.watch(paths.scripts, gulp.parallel(lint));
+    gulp.watch(paths.scripts, gulp.parallel(lint));
 }
 
 // Define tasks from plain functions
