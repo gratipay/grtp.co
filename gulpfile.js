34552b31486351ff9d93760c1cdcdbd63232bb14
anatoly techtonik <techtonik@gmail.com>
2015-11-05 10:03:42 +0300

    ﻿Replace JSHint with ESlint, exercising my new npm-fu
        npm remove gulp-jshint --save
        npm remove jshint-stylish --save
        npm install gulp-eslint --save
    
    It appears that JSON is not valid a JavaScript, so it
    probably needs a separate checker.

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,27 @@
 // Gulp 4
 
 var gulp   = require('gulp');
-var jshint = require('gulp-jshint');
-var stylish = require('jshint-stylish');
+var eslint = require('gulp-eslint');
 
 var paths = {
   scripts: [
     'gulpfile.js',
-    'lib/**/*.{js,json}',
-    'test/**/*.{js,json}'
+    'lib/**/*.js',
+    'test/**/*.js'
   ]
 };
 
 function lint() {
   return gulp.src(paths.scripts)
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish))
-    .pipe(jshint.reporter('fail'))
+     // eslint() attaches the lint output to the eslint property 
+     // of the file object so it can be used by other modules. 
+    .pipe(eslint())
+    // eslint.format() outputs the lint results to the console. 
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) after
+    // lint errors, return the stream and pipe to failAfterError
+    // last. 
+    .pipe(eslint.failAfterError())
     ;
 }
 
