ff9f7865fc4a15abaab4fddd5e5295eb143490af
Matt <bedrocks@gmail.com>
2016-02-11 14:35:19 -0600

    Remove anonymous receiving test
    There is no more anonymous receiving, only anonymous giving.

--- test/test_default-widget-anonymous.js
+++ test/test_default-widget-anonymous.js
@@ -7,19 +7,6 @@ module.exports = {
             }, [], 2000)
             .assert.attr('[data-gratipay-username]', 'data-gratipay-readystatus').is('ready', 'readystatus should be ready')
             .done();
-    },
-
-    'default-widget-anonymous: it should show anonymous receiving': function(test) {
-        test.open('http://localhost:9537/test/default-widget-anonymous.html')
-            .waitFor(function() {
-                return !!document.querySelector('[data-gratipay-readystatus="ready"]');
-            }, [], 2000)
-            .assert.exists('iframe')
-            .toFrame('iframe')
-                .assert.exists('.gratipay-receiving')
-                .assert.text('.gratipay-receiving').is('anonymously', 'receiving should not be empty')
-            .toParent()
-            .done();
-    },
-
+    }
+	
 };
